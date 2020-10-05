4b972875629842a3e6aeba00b5e5fc6d2d67d34f
NadezhdaKovalenko <36608364+NadezhdaKovalenko@users.noreply.github.com>
2020-10-05 13:31:38 +0300

    add safe in csv format

--- laba7.py
+++ laba7.py
@@ -1,28 +1,27 @@
-import numpy as np
 from tabulate import tabulate
 import scipy.stats as stats
 from scipy.stats import norm
 import matplotlib.pyplot as plt
+import numpy as np
+import csv
 
 
 def normalData(n, a, b):
-    sample = []
-    while len(sample) < n:
-       while True:
-           deviate = norm.rvs(loc=2, scale=1, size=1)[0]
-           if a <= deviate <= b:
-               break
-       sample.append(deviate)
-    print (sample)
-    arrSample = np.array(sample)
-    return arrSample
+    result = np.random.random(n)
+    for i in range(n):
+        result[i] = a + (b-a) * result[i]
+    return result
+
 
 def uniformData(n, a, b):
     return np.random.uniform(a, b, size=n)
 
-def printTable(q, delta, array, result):
-    headers = ["i", "$Delta_i$", "$n_i$", "$p_i$", "$np_i$", "$n_i-np_i$", "$frac{(n_i-np_i)^2}{np_i}$"]
+
+def printTable(q, delta, array, result, fileName):
+    headersCsv = ['i', 'Delta_i','n_i', 'p_i', 'np_i', 'n_i-np_i', 'frac{(n_i-np_i)^2}{np_i}']
     rows = []
+    strRows = []
+     
     for i in range(0, len(q)):
         if i == 0:
             boarders = ['-infity', np.around(delta[0], decimals=2)]
@@ -30,22 +29,54 @@ def printTable(q, delta, array, result):
             boarders = [np.around(delta[-1], decimals=2), 'infity']
         else:
             boarders = [np.around(delta[i - 1], decimals=2), np.around(delta[i], decimals=2)]
-        rows.append([i + 1, boarders, q[i], np.around(array[i], decimals=4), np.around(array[i] * 100, decimals = 2),
-                     np.around(q[i] - 100 * array[i], decimals=2), np.around(result[i], decimals=2)])
-    rows.append([len(q), "-", np.sum(q), np.around(np.sum(array), decimals=4),
-                 np.around(np.sum(array * 100), decimals=2),
-                 -np.around(np.sum(q - 100 * array), decimals=2),
-                 np.around(np.sum(result), decimals=2)])
-    print(tabulate(rows, headers, tablefmt="latex"))
+
+        rows.append([str(i + 1),
+                    str(boarders),
+                    str( q[i]),
+                    str(np.around(array[i], decimals=4)),
+                    str(np.around(array[i] * 100, decimals = 2)),
+                    str(np.around(q[i] - 100 * array[i], decimals=2)),
+                    str(np.around(result[i], decimals=2))])
+
+    rows.append([str(len(q)),
+                "-",
+                str(np.sum(q)),
+                str(np.around(np.sum(array), decimals=4)),
+                str(np.around(np.sum(array * 100), decimals=2)),
+                str(-np.around(np.sum(q - 100 * array), decimals=2)),
+                str(np.around(np.sum(result), decimals=2))])
+
+
+    csv_writer(fileName, headersCsv, rows)
+    #print(tabulate(rows, headersTex, tablefmt="latex"))
+    print("Writing complete")
+
+def csv_writer(path, fieldnames, data):
+    """
+    Функция для записи в файл csv
+    path - путь до файла
+    fieldnames - название столбцов
+    data - список из списков
+    """
+    with open(path, "w", newline='') as out_file:
+        '''
+        out_file - выходные данные в виде объекта
+        delimiter - разделитель :|;
+        fieldnames - название полей (столбцов)
+        '''
+        writer = csv.writer(out_file, delimiter = ";")
+        #writer.writeheader()
+        for row in data:
+            writer.writerow(row)
 
 def printParameters(data):
-    mu = np.mean(data)
-    sigma = np.std(data)
-    result = "mu = " + str(np.around(mu, decimals=2)) + " sigma = " + str(np.around(sigma, decimals=2))
-    print(result)
+    print("mu = " +
+    str(np.around(np.mean(data), decimals=2)) +
+    " sigma = " + str(np.around(np.std(data), decimals=2)))
 
-def countHiSquare(dist, k, table):
-    delta = np.linspace(-2, 2, num=k-1)
+
+def countHiSquare(dist, k, table, fileName, start, finish):
+    delta = np.linspace(start, finish, num=k-1)
     array = np.array([stats.norm.cdf(delta[0])])
     q = np.array([len(dist[dist <= delta[0]])])
     for i in range(0, len(delta) - 1):
@@ -54,11 +85,9 @@ def countHiSquare(dist, k, table):
         q = np.append(q, len(dist[(dist <= delta[i + 1]) & (dist >= delta[i])]))
     array = np.append(array, 1 - stats.norm.cdf(delta[-1]))
     q = np.append(q, len(dist[dist >= delta[-1]]))
-    result = np.divide(np.multiply((q - 100 * array), (q - 100 * array)), array * 100)
-    
+    result = np.divide(np.multiply((q - 100 * array), (q - 100 * array)), array * 100)    
     if table == True:
-        printTable(q, delta, array, result)
-    
+        printTable(q, delta, array, result, fileName)
     return (np.around(np.sum(result), decimals=2))
 
 
@@ -70,20 +99,23 @@ def drawData(data):
 ############################################################################
 alpha = 0.05
 p = 1 - alpha
-k = 6
+k = 501
 
-i = 50
+start = -2
+finish = 2
 
-#for i in range(10,150,5):
+num = 75
+
 print("--------------------------------------------------")
 
-distNorm = normalData(i, -2, 2)
-#drawData(distNorm)
-distUniform = uniformData(i, -2, 2)
-#drawData(distUniform)
+
+distNorm = normalData(num, start, finish)
+distUniform = uniformData(num, start, finish)
 
 value = stats.chi2.ppf(p, k-1)
-hiNorm = countHiSquare(distNorm, k, True)
-hiUniform = countHiSquare(distUniform, k, True)
-result = str(i)+": quantile = "+str(value)+" hiSquareNorm = "+str(hiNorm)+" hiSquareUniform = "+str(hiUniform)
+hiNorm = countHiSquare(distNorm, k, True,'dataNorm.csv', start, finish)
+hiUniform = countHiSquare(distUniform, k, True, 'dataUniform.csv', start, finish)
+result = str(num)+": quantile = "+str(value)+" hiSquareNorm = "+str(hiNorm)+" hiSquareUniform = "+str(hiUniform)
 print(result)
+drawData(distNorm)
+drawData(distUniform)
